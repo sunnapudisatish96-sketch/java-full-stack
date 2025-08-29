b5b9c7728403f52e077b431648a1120484bbd8bf
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-29 16:32:27 +0530

    Add files via upload

---
+++ satish.java
@@ -0,0 +1,41 @@
+package satishit;
+
+public class Demo1 {
+	public static void main (String[] args)
+	{
+		//java.util.ArrayList<Integer>al=new java.util.ArrayList<>();
+		//al.add(10);
+		//al.add(20);
+		//al.add(30);
+		//al.add(40);
+		//System.out.println(al);
+	//}
+
+//}
+		//java.util.LinkedList<Integer>ll=new java.util.LinkedList<>();
+		//ll.add(10);
+		//ll.add(20);
+		//ll.add(30);
+		//ll.add(40);
+		//System.out.println(ll);
+	//}
+//}
+
+		//java.util.HashSet<Integer>hs=new java.util.HashSet<>();
+		//hs.add(7);
+		//hs.add(3);
+		//hs.add(1);
+		//hs.add(5);
+		//System.out.println(hs);
+	//}
+//}
+		//java.util.TreeMap<String,Integer>ts=new java.util.TreeMap<>();
+		//ts.put("Satish", 103);
+		//ts.put("Rajesh", 104);
+		//ts.put("ramu", 105);
+		//ts.put("Ramana", 107);
+		//System.out.println(ts);
+	}
+}
+		
+		
