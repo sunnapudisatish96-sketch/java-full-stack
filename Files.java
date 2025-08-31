849976cd63708cd20ba8acd7a45c6864e28b0093
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-31 08:31:11 +0530

    Add files via upload

---
+++ Files.java
@@ -0,0 +1,32 @@
+package maait;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Application {
+	public static void main(String[] args) throws Exception {
+		for (int i = 0; i <= 3; i++) {
+			// Scanner sc=new Scanner(System.in);
+			// System.out.println("Enter the some text");
+			// String entertext=sc.nextLine();
+			// System.out.println(entertext);
+			// }
+			// }
+
+//}
+			//try {
+
+				//File file = new File("MyFile.txt");
+				//java.util.Scanner input = new java.util.Scanner(file);
+				//while (input.hasNextLine()) {
+					//String line = input.nextLine();
+					//System.out.println(line);
+				//}
+			//} catch (FileNotFoundException e) {
+				//e.printStackTrace();
+
+			//}
+		//}
+	//}
+//}
