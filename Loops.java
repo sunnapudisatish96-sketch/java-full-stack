191e622caace642ede9d30158fa7479e1b5115c0
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-31 08:19:00 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,140 @@
+package maait;
+
+public class Operators {
+	public static void main (String[] args)
+	{
+		int a=10;
+		int b=2;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
+class LogicalOperator
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		if(a==b)
+		{
+			System.out.println("a is equal to the b");
+		}
+		//else if(a!=b)
+		{
+			//System.out.println(" a is the not equal to b");
+		}
+		//else if(a>b)
+		{
+			//System.out.println("a is the greater than b");
+		}
+	}
+}
+package maait;
+
+public class Operators {
+	public static void main (String[] args)
+	{
+		int a=10;
+		int b=2;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
+package maait;
+
+public class ControlFlow {
+	public static void main(String[] args) {
+		// boolean hungry=true;
+		// if(hungry)
+		// {
+		// System.out.println("Im starving");
+		// }
+		// else
+		// {
+		// System.out.println("im not hungry");
+		// }
+		// }
+
+//}
+		// int favouriteTemp=75;
+		// int currentTemp=60;
+		// String opinion;
+		// if(currentTemp<favouriteTemp-20)
+		// {
+		// opinion="it's a pretty Darn cold";
+		// }
+		// else if(currentTemp<favouriteTemp-30)
+		// {
+		// opinion="it's cold out";
+
+		// }
+		// else if(currentTemp<favouriteTemp-10)
+		// {
+		// opinion="it's a hot cold";
+		// }
+		// else
+		// {
+		// opinion="Have a nice day";
+		// System.out.println(opinion);
+		// }
+
+		// }
+//}
+		// int marks = 90;
+		// if (marks <= 85) {
+		// System.out.println("Grade A");
+		// } else if (marks >= 75) {
+		// System.out.println("Grade B");
+		// } else if (marks >= 60) {
+		// System.out.println("Grade c");
+		// } else {
+		// System.out.println("Pass students");
+		// }
+		// }
+
+//}
+		//int day = 3;
+		//switch (day) {
+		//case 1:
+			//System.out.println("Monday");
+			//break;
+		//case 2:
+			//System.out.println("Tuesday");
+			//break;
+		//case 3:
+			//System.out.println("WednesDay");
+			//break;
+		//case 4:
+			//System.out.println("Thursday");
+			//break;
+		//default:
+			//System.out.println("Not found");
+		//}
+	//}
+//}
+		String colour="Red";
+		switch(colour)
+		{
+		case colour:System.out.println("Blue");
+		break;
+		case colour:System.out.println("Pink");
+		break;
+		case colour:System.out.println("Purple");
+		break;
+		case colour:System.out.println("Red");
+		break;
+		default:
+			System.out.println("Not found");
+		
+		}
+	}
+}
+
