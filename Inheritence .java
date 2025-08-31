2fced7abfc3a408f1eb6d7b47e630ae7c309b6a1
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-31 08:27:52 +0530

    Add files via upload

---
+++ Inheritence .java
@@ -0,0 +1,32 @@
+package maait;
+
+//class Satish{
+	//String name;
+	//int id;
+	//int age;
+	//double marks;
+
+	//public void Show() {
+		//System.out.println("This is the show method");
+		//System.out.println("Name:" + name);
+		//System.out.println("Id:" + id);
+		//System.out.println("Age:" + age);
+		//System.out.println("Marks:" + marks);
+	//}
+//}
+
+	//public class Inheritence extends Satish {
+		//int rank;
+
+		//public static void main(String[] args) {
+			//Inheritence s = new ();
+			//s.name = "raju";
+			//s.id = 105;
+			//s.age = 23;
+			//s.marks = 90.0;
+			//s.rank = 1;
+			//System.out.println("Rank:" + s.rank);
+			//s.Show();
+		//}
+	//}
+
