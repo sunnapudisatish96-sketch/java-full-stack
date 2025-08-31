01fc092f89808261d1cb601af3e3a4ae87c2b8d5
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-31 08:26:16 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,21 @@
+package maait;
+
+public class Methods {
+	public static void main (String[] args)
+	{
+		System.out.println("learning methods");
+		//printSomeJunk("Satish");
+		//printSomeJunk(23);
+		int result= add(20,30);
+		System.out.println("sum is:"+result);
+	}
+	//public static void printSomeJunk(String argument)
+	//{
+		//System.out.println("some methods:"+argument);
+	//}
+	//public static void printSomeJunk(int arguments)
+	//{
+		//System.out.println("Integer passed in");
+	//}
+
+//}
