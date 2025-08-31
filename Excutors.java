1934f18976e5e54b824df566a8da9719407cc686
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-31 08:48:23 +0530

    Add files via upload

---
+++ Excutors.java
@@ -0,0 +1,38 @@
+package satishit;
+
+//import java.util.concurrent.Callable;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+
+//public class Demo implements Callable {
+	//public Object call() throws Exception {
+		//System.out.println("call method are the called");
+		//return "Satish";
+	//}
+
+	//public static void main(String[] args) {
+		//Demo d = new Demo();
+		//ExecutorService exService = Executors.newFixedThreadPool(10);
+		//for (int i = 0; i <= 15; i++) {
+			//exService.submit(d);
+		//}
+		//exService.shutdown();
+	//}
+//}
+//class Dem1 implements Runnable
+//{
+	//public void run()
+	//{
+		//System.out.println("run()method called");
+		
+	//}
+	//public static void main (String[] args)
+	//{
+	//Dem1 d=new Dem1();
+	//Thread t=new Thread(d);
+//t.run();
+	//}
+		
+	//}
+	//}
+//}
