7c539e705828f8eab8af20a6618afe2ce373338b
sunnapudisatish96-sketch <sunnapudisatish96@gmail.com>
2025-08-31 08:11:48 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,84 @@
+package maait;
+
+public class DataType {
+	public static void main (String[] args)
+	{
+		int var=-1000000000;
+		System.out.println(var);
+		short bignumber =-32768;
+		System.out.println(bignumber);
+		byte realsmallnumber=-128;
+		System.out.println(realsmallnumber);
+		double decimalnumber=987.00;
+		System.out.println(decimalnumber);
+		boolean decision=true;
+		System.out.println(decision);
+		char letter='P';
+		System.out.println(letter);
+	}
+
+}
+=========Strings============
+package maait;
+
+public class StringDemo {
+	public static void main (String[] args)
+	{
+		String str="Satish";
+		String str2=str.substring(1);
+		System.out.println(str2);
+	}
+	}
+		//String str="Satish";
+		//str.concat("s");
+		//System.out.println(str);
+		//StringBuffer str2=new StringBuffer("Satish");
+		//str2.append("s");
+		//System.out.println(str2);
+		//String str1="Satish";
+		//String str2="Akka";
+		//str1.concat(str2);
+		//System.out.println(str1);
+		//String str="Satish";
+		//String str1=str.substring(4,5);
+		//System.out.println(str1);
+		
+		//String str1="Hello world";
+		//String replace=str1.replace("Hello","java");
+		//System.out.println(replace);
+		//String str1="satish learn here.. lead anywhere";
+		//String string=str1.toString();
+		//System.out.println(string);
+//String str1="Machilipatnam";
+//System.out.println(str1.indexOf("M"));
+//System.out.println(str1.lastIndexOf("m"));
+		//String str1="Satish";
+		//boolean contains1=str1.contains("it");
+		//boolean contains2=str1.contains("it");
+		//System.out.println(contains2);
+		
+		//String str1="Satish";
+		//String str2="it";
+		//int compareTo=str1.compareTo(str2);
+		
+		//System.out.println(compareTo);
+		//String str="Satish";
+		//char charAt=str.charAt(3);
+		//System.out.println(charAt);
+		//String str1="Rajesh";
+		//char charAt=str1.charAt(2);
+		//System.out.println(charAt);
+		//String str1="Satish";
+		//String str2="Rajesh";
+		//String str3=str1.concat(str2);
+		//System.out.println(str3);
+		//String str="Satish";
+		//System.out.println(str.length());
+		
+		
+	//}
+
+//}
+
+
+//}
